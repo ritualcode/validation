6685d8477a713b0bdcdd61f8a766e69bad530e3f
ritualcode <ritualcode@gmail.com>
2015-04-04 13:34:02 +0300

    version 2.0 April,4

--- js/email.js
+++ js/email.js
@@ -1,23 +1,74 @@
-// объект e-mail с методами создать (подгрузить html) и удалить
+// объект email с методами 
+// create (подгрузить html и вызвать методы stylize и required: см.ниже)
+// destroy (удалить элемент)
+// stylize (стилизовать в соответствии с найтройками)
+// required (вставить звездочку если в настройках required.value == true)
+// _validate (проверить maxlenght)
+// submit (обработчик события сабмит для формы, который вызывает validate)
 var email = {
 	create: function(){
 		$.get("../html/email.html", function(htmlCode){
 			$("form").append(htmlCode);
+			email.required();
+			email.stylize();
 		});
 	},
 	destroy: function(){
 		$(".email-group").remove();
+	},
+	stylize: function(){
+		$("#email").css(emailOptions.styles.input);
+		$(".email-message").css(emailOptions.styles.message);
+		$(".glyphicon-star").css(emailOptions.styles.star)
+	},
+	required: function() {
+		$("#email").ready(function(){
+			var reqIcon = $("<span class='glyphicon glyphicon-star form-control-feedback'></span>");
+			if (emailOptions.validation.required.value) {
+				$("#email").after(reqIcon);
+			}
+		});	
+	},
+	_validate: function(){
+		var emailBox = $("#email");
+		var emailMessage = $(".email-message");
+		if( emailBox.val().length > emailOptions.validation.maxlength.value ){
+			emailMessage.text(emailOptions.validation.maxlength.message);
+			return false;
+		}else{
+			return true;
+		}
+	},
+	submit: function(){
+		$("form").on("submit", function(event){
+		event.preventDefault();
+		email._validate();
+		});
 	}
 }
 
-// объект настроек e-mail с правилом валидации maxlength и стилем для валидного и невалидного значения
-var email_options = {
+// объект настроек e-mail с правилом валидации 
+// required и maxlength и дефолтными стилем
+var emailOptions = {
 	validation: {
-		maxlength: 8,
-		message: "Email is too long"
+		required: {
+			value: true
+		},
+		maxlength: {
+			value: 8,
+			message: "Your e-mail is too long"
+		}
 	},
 	styles: {
-		error: "has-error", //bootstrap style
-		success: "has-success" //bootstrap style
+		input: {
+			"background": "#ccc",
+			"font-weight": "bold"
+		},
+		message: {
+			"color": "red"
+		},
+		star: {
+			"color": "blue"
+		}
 	}
 }
--- js/password.js
+++ js/password.js
@@ -1,23 +1,71 @@
-// объект password с методами создать (подгрузить html) и удалить
+// объект password с методами 
+// create (подгрузить html и вызвать методы stylize и required: см.ниже)
+// destroy (удалить элемент)
+// stylize (стилизовать в соответствии с найтройками)
+// required (вставить звездочку если в настройках required.value == true)
+// _validate (проверить maxlenght)
+// submit (обработчик события сабмит для формы, который вызывает validate)
 var password = {
 	create: function(){
 		$.get("../html/password.html", function(htmlCode){
 			$("form").append(htmlCode);
+			password.required();
+			password.stylize();
 		});
 	},
 	destroy: function(){
 		$(".password-group").remove();
+	},
+	stylize: function(){
+		$("#password").css(passwordOptions.styles.input);
+		$(".password-message").css(emailOptions.styles.message);
+		$(".glyphicon-star").css(emailOptions.styles.star)
+	},
+	required: function() {
+		var reqIcon = $("<span class='glyphicon glyphicon-star form-control-feedback'></span>");
+		if (passwordOptions.validation.required.value) {
+			$("#password").after(reqIcon);
+		}
+	},
+	_validate: function() {
+		var passwordBox = $("#password");
+		var passwordMessage = $(".password-message");
+		if( passwordBox.val().length > passwordOptions.validation.maxlength.value ){
+				passwordMessage.text(passwordOptions.validation.maxlength.message);
+				return false
+		}else{
+			return true;
+		}
+	},
+	submit: function() {
+		$("form").on("submit", function(event){
+			event.preventDefault();
+			password._validate();
+		});
 	}
 }
 
-// объект настроек e-mail с правилом валидации maxlength и стилем для валидного и невалидного значения
-var password_options = {
+// объект настроек password с правилом валидации 
+// required и maxlength и дефолтными стилем
+var passwordOptions = {
 	validation: {
-		maxlength: 8,
-		message: "Password is too long"
+		required: {
+			value: true
+		},
+		maxlength: {
+			value: 8,
+			message: "Password is too long"
+		}
 	},
 	styles: {
-		error: "has-error", //bootstrap style
-		success: "has-success" //bootstrap style
+		input: {
+			"background": "#ccc",
+		},
+		message: {
+			"color": "red"
+		},
+		star: {
+			"color": "blue"
+		}
 	}
 }
